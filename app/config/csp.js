f2e3b71e3893330210d4215abf8b3c4e91cc1586
Dan Moneypenny <catullus1973@gmail.com>
2021-01-05 16:45:26 -0800

    update

--- app/config/csp.js
+++ app/config/csp.js
@@ -1,7 +1,7 @@
 const policies = {
   directives: {
     "default-src": ["'self'"],
-    "img-src": ["'self'", "*.cloudinary.com"],
+    "img-src": ["'self'", "*.cloudinary.com", "data:"],
     "font-src": ["'self'", "https://fonts.googleapis.com", "https://fonts.gstatic.com"],
     "style-src-elem": ["'self'", "https://fonts.googleapis.com", "https://fonts.gstatic.com"],
     "style-src": ["'self'"]
