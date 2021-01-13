a45001cefc096cd1fd573104829259a34d8d32c0
Dan Moneypenny <catullus1973@gmail.com>
2021-01-12 21:41:09 -0800

    change landing page

--- app/_controllers/client/routes.landing.js
+++ app/_controllers/client/routes.landing.js
@@ -1,17 +1,39 @@
 const { Router } = require('express');
 const landingsRouter = Router();
 
+// models
+const Project = require('../../_models/Project');
+
 // controllers
 const {
   landing,
   about,
   contact,
   terms
-} = require('./controllers.landing')
+} = require('./controllers.landing');
+
+const { 
+  projects_get 
+} = require('./controllers.projects');
+
+// middleware
+const handleQuery = require('../middleware/handleQuery');
+
+// populate options
+const populate = [
+  { path: 'items', select: 'title' },
+  { path: 'milestones', select: 'date, title'},
+  { path: 'tasks', select: 'data, title'}
+
+];
 
 landingsRouter
   .route('/')
-  .get(landing);
+  .get(handleQuery(Project, populate), projects_get);
+
+landingsRouter
+  .route('/home')
+  .get(landing);  
 
 landingsRouter
   .route('/about')
