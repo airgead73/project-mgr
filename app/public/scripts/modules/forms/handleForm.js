4fce1285a44ddd923548405ef9459a3b122bd0af
Dan Moneypenny <catullus1973@gmail.com>
2021-01-20 03:39:59 -0800

    Set up method handler.

--- app/public/scripts/modules/forms/handleForm.js
+++ app/public/scripts/modules/forms/handleForm.js
@@ -1,88 +1,117 @@
-import { showSuccess, showFailure } from '../messages/show.js';
-
+//import { showSuccess, showFailure } from '../messages/show.js';
+import { handleGet, handlePost, handlePut, handleDelete} from './handleMethods.js';
 export default function handleForm(_btns) {
 
-  const handleClick = (_btn) => {
+  const assignMethodHandler = (_target, _method) => {
 
-    // get form attributes
-    const form = _btn.form;
-    const contentType = form.getAttribute('enctype');
-    const method = form.getAttribute('method');
-    const url = form.getAttribute('action');
-    const responseRedirect = form.getAttribute('target');
-    const messageContainer = form.querySelector('[data-message="container"]');
+    switch(_method) {
+      case 'GET':
+        handleGet();
+        break;
+      case 'POST':
+        handlePost();
+        break; 
+      case 'PUT':
+        handlePut();
+        break;
+      case 'DELETE':
+        handleDelete();
+        break;
+      default:
+        console.error('No method available.');
+        break;                               
 
-    // set request headers
-    const currentHeaders = new Headers();
-
-    if(contentType === 'application/json') {
-      currentHeaders.append('Content-Type', contentType);
-    }
-    
-    currentHeaders.append('Accept', contentType);
-
-    // set request body
-
-    let body;
-
-    if(contentType === 'application/json') {
-      let formElements = Array.from(form.elements);
-
-      formElements = formElements.filter(el => {
-        if(el.hasAttribute('name')) {
-          return el
-        }
-      });
-
-      body = {};
-  
-      formElements.forEach(el => {
-        const prop = el.getAttribute('name');
-        const value = form[prop].value;
-        body[prop] = value;
-      });
-  
-      body = JSON.stringify(body);     
-    } else if(contentType === 'multipart/form-data') {
-      body = new FormData(form);
     }
 
-    // fetch data
-    fetch(url, {
-      method: method,
-      headers: currentHeaders,
-      body: body     
-    })
-      .then(response => response.json())
-      .then(data => {
-        const { success, msg } = data;
-        if(success) {
-
-          if(method === 'PUT') {
-            location.assign(responseRedirect);
-          } else if(method === 'POST') {
-            form.reset();
-            console.log(data);
-            showSuccess(messageContainer, msg);
-          }
-
-        } else {
-
-          console.log(data.messages);
-          //showFailure(messageContainer, data.messages);
-
-        }
-        
-      });
   }
 
-  _btns.forEach(_btn => {
+  _btns.forEach(_btn => {    
 
     _btn.addEventListener('click', (e) => {
+
       e.preventDefault();
-      handleClick(e.target);      
+      const target = e.target
+      const method = target.getAttribute('formmethod') || null;
+      assignMethodHandler(target, method);
+           
     });
 
-  });
+  });  
+
+  // const handleClick = (_btn) => {
+
+  //   // get form attributes
+  //   const form = _btn.form;
+  //   const contentType = form.getAttribute('enctype');
+  //   const method = form.getAttribute('method');
+  //   const url = form.getAttribute('action');
+  //   const responseRedirect = form.getAttribute('target');
+  //   const messageContainer = form.querySelector('[data-message="container"]');
+
+  //   // set request headers
+  //   const currentHeaders = new Headers();
+
+  //   if(contentType === 'application/json') {
+  //     currentHeaders.append('Content-Type', contentType);
+  //   }
+    
+  //   currentHeaders.append('Accept', contentType);
+
+  //   // set request body
+
+  //   let body;
+
+  //   if(contentType === 'application/json') {
+  //     let formElements = Array.from(form.elements);
+
+  //     formElements = formElements.filter(el => {
+  //       if(el.hasAttribute('name')) {
+  //         return el
+  //       }
+  //     });
+
+  //     body = {};
+  
+  //     formElements.forEach(el => {
+  //       const prop = el.getAttribute('name');
+  //       const value = form[prop].value;
+  //       body[prop] = value;
+  //     });
+  
+  //     body = JSON.stringify(body);     
+  //   } else if(contentType === 'multipart/form-data') {
+  //     body = new FormData(form);
+  //   }
+
+  //   // fetch data
+  //   fetch(url, {
+  //     method: method,
+  //     headers: currentHeaders,
+  //     body: body     
+  //   })
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       const { success, msg } = data;
+  //       if(success) {
+
+  //         if(method === 'PUT') {
+  //           location.assign(responseRedirect);
+  //         } else if(method === 'POST') {
+  //           form.reset();
+  //           console.log(data);
+  //           //showSuccess(messageContainer, msg);
+  //         }
+
+  //       } else {
+
+  //         console.log(data.messages);
+  //         //showFailure(messageContainer, data.messages);
+
+  //       }
+        
+  //     });
+  // }
+
+
 
 }
---
+++ app/public/scripts/modules/forms/handleMethods.js
@@ -0,0 +1,15 @@
+export function handleGet() {
+  console.log('GET request');
+}
+
+export function handlePost() {
+  console.log('POST request');
+}
+
+export function handlePut() {
+  console.log('PUT request');
+}
+
+export function handleDelete() {
+  console.log('DELETE request');
+}
