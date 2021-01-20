dab122634663b9ca7bb40253d4515adf0b19ff4e
Dan Moneypenny <catullus1973@gmail.com>
2021-01-20 07:05:11 -0800

    Add post method handler

--- app/public/scripts/modules/forms/handleForm.js
+++ app/public/scripts/modules/forms/handleForm.js
@@ -6,16 +6,16 @@ export default function handleForm(_btns) {
 
     switch(_method) {
       case 'GET':
-        handleGet();
+        handleGet(_target);
         break;
       case 'POST':
-        handlePost();
+        handlePost(_target);
         break; 
       case 'PUT':
-        handlePut();
+        handlePut(_target);
         break;
       case 'DELETE':
-        handleDelete();
+        handleDelete(_target);
         break;
       default:
         console.error('No method available.');
@@ -38,79 +38,79 @@ export default function handleForm(_btns) {
 
   });  
 
-  // const handleClick = (_btn) => {
+  const handleClick = (_btn) => {
 
-  //   // get form attributes
-  //   const form = _btn.form;
-  //   const contentType = form.getAttribute('enctype');
-  //   const method = form.getAttribute('method');
-  //   const url = form.getAttribute('action');
-  //   const responseRedirect = form.getAttribute('target');
-  //   const messageContainer = form.querySelector('[data-message="container"]');
+    // get form attributes
+    const form = _btn.form;
+    const contentType = form.getAttribute('enctype');
+    const method = form.getAttribute('method');
+    const url = form.getAttribute('action');
+    const responseRedirect = form.getAttribute('target');
+    const messageContainer = form.querySelector('[data-message="container"]');
 
-  //   // set request headers
-  //   const currentHeaders = new Headers();
+    // set request headers
+    const currentHeaders = new Headers();
 
-  //   if(contentType === 'application/json') {
-  //     currentHeaders.append('Content-Type', contentType);
-  //   }
+    if(contentType === 'application/json') {
+      currentHeaders.append('Content-Type', contentType);
+    }
     
-  //   currentHeaders.append('Accept', contentType);
+    currentHeaders.append('Accept', contentType);
 
-  //   // set request body
+    // set request body
 
-  //   let body;
+    let body;
 
-  //   if(contentType === 'application/json') {
-  //     let formElements = Array.from(form.elements);
+    if(contentType === 'application/json') {
+      let formElements = Array.from(form.elements);
 
-  //     formElements = formElements.filter(el => {
-  //       if(el.hasAttribute('name')) {
-  //         return el
-  //       }
-  //     });
+      formElements = formElements.filter(el => {
+        if(el.hasAttribute('name')) {
+          return el
+        }
+      });
 
-  //     body = {};
+      body = {};
   
-  //     formElements.forEach(el => {
-  //       const prop = el.getAttribute('name');
-  //       const value = form[prop].value;
-  //       body[prop] = value;
-  //     });
+      formElements.forEach(el => {
+        const prop = el.getAttribute('name');
+        const value = form[prop].value;
+        body[prop] = value;
+      });
   
-  //     body = JSON.stringify(body);     
-  //   } else if(contentType === 'multipart/form-data') {
-  //     body = new FormData(form);
-  //   }
+      body = JSON.stringify(body);     
+    } else if(contentType === 'multipart/form-data') {
+      body = new FormData(form);
+    }
 
-  //   // fetch data
-  //   fetch(url, {
-  //     method: method,
-  //     headers: currentHeaders,
-  //     body: body     
-  //   })
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       const { success, msg } = data;
-  //       if(success) {
+    // fetch data
+    fetch(url, {
+      method: method,
+      headers: currentHeaders,
+      body: body     
+    })
+      .then(response => response.json())
+      .then(data => {
+        const { success, msg } = data;
+        if(success) {
 
-  //         if(method === 'PUT') {
-  //           location.assign(responseRedirect);
-  //         } else if(method === 'POST') {
-  //           form.reset();
-  //           console.log(data);
-  //           //showSuccess(messageContainer, msg);
-  //         }
+          if(method === 'PUT') {
+            location.assign(responseRedirect);
+          } else if(method === 'POST') {
+            form.reset();
+            console.log(data);
+            //showSuccess(messageContainer, msg);
+          }
 
-  //       } else {
+        } else {
 
-  //         console.log(data.messages);
-  //         //showFailure(messageContainer, data.messages);
+          console.log(data.messages);
+          //showFailure(messageContainer, data.messages);
 
-  //       }
+        }
         
-  //     });
-  // }
+      });
+  }
 
 
 
--- app/public/scripts/modules/forms/handleMethods.js
+++ app/public/scripts/modules/forms/handleMethods.js
@@ -2,8 +2,78 @@ export function handleGet() {
   console.log('GET request');
 }
 
-export function handlePost() {
-  console.log('POST request');
+export function handlePost(_btn) {
+  const form = _btn.form;
+  const contentType = form.getAttribute('enctype');
+  const url = form.getAttribute('action');
+  const responseRedirect = form.getAttribute('target');
+  const messageContainer = form.querySelector('[data-message="container"]'); 
+
+  // set request headers
+  const currentHeaders = new Headers();
+
+  if(contentType === 'application/json') {
+    currentHeaders.append('Content-Type', contentType);
+  }
+    
+  currentHeaders.append('Accept', contentType);
+
+  // set request body
+
+  let body;
+
+  if(contentType === 'application/json') {
+    let formElements = Array.from(form.elements);
+
+    formElements = formElements.filter(el => {
+      if(el.hasAttribute('name')) {
+        return el
+      }
+    });
+
+    body = {};
+  
+    formElements.forEach(el => {
+      const prop = el.getAttribute('name');
+      const value = form[prop].value;
+      body[prop] = value;
+    });
+  
+  body = JSON.stringify(body);     
+   } else if(contentType === 'multipart/form-data') {
+      body = new FormData(form);
+  }  
+
+  // fetch data
+  fetch(url, {
+    method: method,
+    headers: currentHeaders,
+    body: body     
+  })
+    .then(response => response.json())
+    .then(data => {
+      const { success, msg } = data;
+      if(success) {
+
+        if(method === 'PUT') {
+          location.assign(responseRedirect);
+        } else if(method === 'POST') {
+          form.reset();
+          console.log(data);
+          //showSuccess(messageContainer, msg);
+        }
+
+      } else {
+
+        console.log(data.messages);
+        //showFailure(messageContainer, data.messages);
+
+      }
+        
+    });  
+
+
+
 }
 
 export function handlePut() {
