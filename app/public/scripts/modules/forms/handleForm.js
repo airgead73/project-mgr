95debd3eea821af1adae8d3fc0309a3952f6291e
Dan Moneypenny <catullus1973@gmail.com>
2021-01-22 08:17:34 -0800

    add switch to handleResponse

--- app/public/scripts/modules/forms/handleForm.js
+++ app/public/scripts/modules/forms/handleForm.js
@@ -1,30 +1,62 @@
 import { showSuccess, showFailure } from '../messages/show.js';
-import { handleGet, handlePost, handlePut, handleDelete} from './handleMethods.js';
+import { handleGet, handlePost, handlePut, handleDelete } from './handleMethods.js';
 export default function handleForm(_btns) {
 
   const handleResponse = (_form, _data) => {
     const { success, msg } = _data;
     const method = _form.getAttribute('method');
 
-    if(method === 'DELETE' && success) {
-
-      return window.location.reload();
-
+    switch(method) {
+      case 'GET':
+        console.log('GET request');
+        break;
+      case 'POST':
+        const messageContainer = _form.querySelector('[data-message="container"]') 
+        if(success) {
+       
+          _form.reset();
+          console.log(_data);
+          showSuccess(messageContainer, msg);
+    
+          } else {
+    
+          console.error(_data.messages);
+          showFailure(messageContainer, _data.messages);
+    
+          }
+        break;
+      case 'PUT':
+        console.log('PUT request');
+        const relocateTo = _form.getAttribute('target');
+        if(success) {
+          window.location.replace(relocateTo);
+        } else {
+          console.error(_data.messages);
+        }
+        break;
+      case 'DELETE':
+        if(success) {
+          window.location.reload();
+        } else {
+          console.error(_data.messages);
+        }
+        break;
+      default:
+        if(success) {
+       
+          _form.reset();
+          console.log(_data);
+          showSuccess(messageContainer, msg);
+    
+          } else {
+    
+          console.error(_data.messages);
+          showFailure(messageContainer, _data.messages);
+    
+          }
+        break;
     }
-
-    const messageContainer = _form.querySelector('[data-message="container"]') 
-    if(success) {
-   
-      _form.reset();
-      console.log(_data);
-      showSuccess(messageContainer, msg);
-
-      } else {
-
-      console.log(_data.messages);
-      showFailure(messageContainer, _data.messages);
-
-      }    
+ 
   }
 
   const assignMethodHandler = (_form) => {
@@ -39,7 +71,7 @@ export default function handleForm(_btns) {
         handlePost(_form).then(data => handleResponse(_form, data));
         break; 
       case 'PUT':
-        handlePut(_form);
+        handlePut(_form).then(data => handleResponse(_form, data));
         break;
       case 'DELETE':
         handleDelete(_form).then(data => handleResponse(_form, data));
--- app/public/scripts/modules/forms/handleMethods.js
+++ app/public/scripts/modules/forms/handleMethods.js
@@ -53,12 +53,58 @@ const handlePost = async function(_form) {
 
   return data;
 
-
-
 }
 
-const handlePut = function(_form) {
-  console.log('PUT request');
+const handlePut = async function(_form) {
+  const contentType = _form.getAttribute('enctype');
+  const url = _form.getAttribute('action');
+
+  // set request headers
+  const currentHeaders = new Headers();
+
+  if(contentType === 'application/json') {
+    currentHeaders.append('Content-Type', contentType);
+  }
+    
+  currentHeaders.append('Accept', contentType);
+
+  // set request body
+
+  let body;
+
+  if(contentType === 'application/json') {
+    let formElements = Array.from(_form.elements);
+
+    formElements = formElements.filter(el => {
+      if(el.hasAttribute('name')) {
+        return el
+      }
+    });
+
+    body = {};
+  
+    formElements.forEach(el => {
+      const prop = el.getAttribute('name');
+      const value = _form[prop].value;
+      body[prop] = value;
+    });
+  
+  body = JSON.stringify(body);     
+   } else if(contentType === 'multipart/form-data') {
+      body = new FormData(_form);
+  }  
+
+  // fetch
+
+  let response = await fetch(url, {
+    method: 'PUT',
+    headers: currentHeaders,
+    body: body   
+  });
+
+  let data = await response.json();
+
+  return data;
 }
 
 const handleDelete = async function(_form) {
